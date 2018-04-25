c83615c743b8bb6ab958a7aa5bdbdac259fc7a03
Odilon Gonzalez <douglasmetalicio@gmail.com>
2018-04-25 15:45:49 +0000

    Se agrega la variable para el incremento

--- Ejemplo-1-Calentador.cpp
+++ Ejemplo-1-Calentador.cpp
@@ -4,18 +4,21 @@ class Calentador
 {
 private:
     int temperatura;
+    int incremento;
     
 public:
     Calentador();
     void calentar();
     void enfriar();
     void imprimeTemperatura();
+    int accedeTemperatura() const; // Metodo de acceso, este metodo no modifica las variables, se le agrega el const a este tipo de metodos
     
 }; //Poner ; al final de la clase
 
 Calentador::Calentador() // "::" Operador de alcance--> a que clase pertenece
 {
     temperatura = 15;
+    incremento = 3;
 }
 
 void Calentador::calentar()
@@ -33,13 +36,18 @@ void Calentador::imprimeTemperatura()
     std::cout << "La temperatura es " << temperatura << std::endl;
 }
 
+int Calentador::accedeTemperatura() const // Lo que regresa "int" a que clase pertenece "::" 
+{
+    
+}
+
 int main()
 {
     
     Calentador C1;
     
     C1.calentar();
-    C1.imprimeTemperatura(); // Se escribe con el punto por que esun metodo
+    C1.imprimeTemperatura(); // Se escribe con el punto por que es un metodo
     
     
     C1.enfriar();
