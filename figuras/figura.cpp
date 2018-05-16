ffc34000a3b819d86cca560e082f26b311b89a3d
Odilon Gonzalez <douglasmetalicio@gmail.com>
2018-05-16 15:52:33 +0000

    solo fue el principio

---
+++ figuras/figura.cpp
@@ -0,0 +1,18 @@
+#include "figura.h" // "" para mis archivos
+#include <iostream> //<> para archivos de C++
+
+Figura::Figura()
+{
+    area = 0;
+
+}
+
+void Figura::calcularArea()
+{
+    std::cout << "NO SE COMO CALCULAR AREA";
+}
+
+void Figura::imprimirArea()
+{
+    std::cout << "EL AREA ES: ";
+}
